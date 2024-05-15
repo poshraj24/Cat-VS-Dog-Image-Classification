4c22c608ed2fea2afece789ba6f25c8ba4e5d893
Posh Raj Dahal <dahal.poshraj24@gmail.com>
2024-05-15 23:24:01 +0200

    importing the necessary modules

--- classifier.py
+++ classifier.py
@@ -1,1 +1,25 @@
 #dense layer for classification
+# IMPORT REQUIRED LIBRARIES:
+# --------------------------
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+import pickle
+import numpy as np
+from numpy import genfromtxt
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import cv2
+from PIL import Image
+import imageio
+import pandas as pd
+import random
+import pickle
+import pandas as pd
+from sklearn.preprocessing import LabelBinarizer
+from skimage.io import imread_collection
+import glob
+
+#Reading .jpeg and .png images from the dataset 
