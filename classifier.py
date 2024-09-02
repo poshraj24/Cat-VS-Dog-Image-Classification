23efd7a1078d2fc59a1590d6416f33c470bb8810
Posh Raj Dahal <dahal.poshraj24@gmail.com>
2024-09-02 12:52:04 +0200

    model development

--- classifier.py
+++ classifier.py
@@ -37,3 +37,43 @@ images= [cv2.imread(file) for file in files]
 X=pickle.load(open("X.pickle","rb"))
 y=pickle.load(open("y.pickle","rb"))
 
+#normalizing the data
+X=np.array(X)
+X=X/255.0
+y=np.array(y)
+
+#Building the model
+model=Sequential()
+model.add(Conv2D(64,(3,3),input_shape=X.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Conv2D(32,(2,2)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+
+#adding sigmoid activation
+model.add(Activation("sigmoid"))
+model.add(Dense(1))
+model.add(Activation("sigmoid"))
+model.compile(loss="binary_crossentropy",optimizer="adam",metrics=["accuracy"])
+
+#fitting the model
+model.fit(X,y,batch_size=32,epochs=50,shuffle=True, sample_weight=None, validation_split=0.1, verbose=1)
+
+#Graphical Representation
+plt.plot(model.history.history['accuracy'])
+plt.plot(model.history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+
+
+
+
